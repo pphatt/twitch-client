588f7e4d6af6e1e1736ff7490fb7f7afa7328f19
pphatt <vutienphat0809@gmail.com>
2024-07-27 00:58:03 +0700

    feat(chat-setting): add hide chat

--- components/stream/chat/user-chat-setting-option/hide-chat.tsx
+++ components/stream/chat/user-chat-setting-option/hide-chat.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { useChatObserver } from "@/store/state/channel-chat"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -9,9 +10,23 @@ import {
 import styles from "@/styles/components/stream/chat/user-chat-setting-option/hide-chat.module.scss"
 
 export default function HideChat() {
+  const { hide, setHide } = useChatObserver()
+
+  const handleHideChat = React.useCallback(() => {
+    if (hide) {
+      setHide(false)
+      return
+    }
+
+    setHide(true)
+  }, [hide, setHide])
+
   return (
     <DropdownMenuItemWrapper>
-      <Button className={styles["chat-setting-button"]}>
+      <Button
+        className={styles["chat-setting-button"]}
+        onClick={handleHideChat}
+      >
         <DropdownMenuItemContainer>
           <DropdownMenuItemTitle>Hide Chat</DropdownMenuItemTitle>
         </DropdownMenuItemContainer>
