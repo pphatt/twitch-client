a1fddb029bb42c6615f5a6615e6fc6151fcd7912
pphatt <vutienphat0809@gmail.com>
2024-08-07 01:10:51 +0700

    fix: fix deployment

--- components/placeholder/category-placeholder/category-placeholder.tsx
+++ components/placeholder/category-placeholder/category-placeholder.tsx
@@ -2,9 +2,7 @@
 
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
 import { PlaceHolder } from "@/components/placeholder/category-placeholder/style"
-import styles from "@/styles/components/placeholder/category-placeholder.module.scss"
 
 interface CategoryPlaceholderProps
   extends React.HTMLAttributes<HTMLDivElement> {}
--- components/placeholder/live-channel-placeholder/live-channel-placeholder.tsx
+++ components/placeholder/live-channel-placeholder/live-channel-placeholder.tsx
@@ -2,9 +2,7 @@
 
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
 import { Placeholder } from "@/components/placeholder/live-channel-placeholder/style"
-import styles from "@/styles/components/placeholder/live-channel-placeholder.module.scss"
 
 interface LiveChannelPlaceholderProps
   extends React.HTMLAttributes<HTMLDivElement> {}
