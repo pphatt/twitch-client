e8bdfc17b294924e25d2459aa4adac4b7c07fb83
pphatt <vutienphat0809@gmail.com>
2024-08-09 20:13:11 +0700

    refactor: prettier code

--- components/ui/aspect-ratio/style.ts
+++ components/ui/aspect-ratio/style.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
-import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
+import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio"
+import styled from "styled-components"
 
 export const AspectRatioWrapper = styled(AspectRatioPrimitive.Root)``
